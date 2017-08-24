f101cd372b9363dbd2a3afc2ed5ec273285f63e3
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-24 17:58:47 +0200

    Push html pages on the website's repo

--- deploy.js
+++ deploy.js
@@ -6,8 +6,9 @@ ghpages.publish(
     user: {
       name: 'OpenFisca-Bot',
     },
-    branch: 'html-branch-publish',
+    branch: 'doc-branch-publish',
     message: 'Auto-commit from master branch',
+    repo: 'https://github.com/openfisca/openfisca.org.git',
   },
   function(err) {
     if (err) {
