9e3d05ae5ed6849efed61035beea68a5491c88d9
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-30 14:57:32 +0200

    Set production branches

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone -b doc-branch-publish https://github.com/openfisca/openfisca.org.git
+git clone -b gh-pages https://github.com/openfisca/openfisca.org.git
 mv _book doc
 rm -rf openfisca.org/doc
 mv doc openfisca.org/doc
@@ -9,7 +9,7 @@ git add .
 git config --global user.name "OpenFisca-Bot"
 git config --global user.email "contact@openfisca.fr"
 git commit -m "Push from openfisca doc"
-git push https://github.com/openfisca/openfisca.org.git doc-branch-publish
+git push https://github.com/openfisca/openfisca.org.git gh-pages
 if git status -uno ; then
 	echo "There was an issue pushing to openfisca.org"
 fi
@@ -17,4 +17,4 @@ fi
 git filter-branch --tree-filter 'rm -rf $(git ls-files | egrep -v doc)' -- --all
 git add .
 git commit -m "Push from openfisca doc"
-git push https://github.com/openfisca/openfisca.org.git doc-branch-publish:doc-html -f
+git push https://github.com/openfisca/openfisca.org.git gh-pages:doc-html -f
