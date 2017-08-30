d9685256626c2317e09d360fd5183e7791dde28b
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-30 21:26:25 +0200

    Simplify procedure

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ mv _book doc
 rm --recursive --force openfisca.org/doc
 mv doc openfisca.org/doc
 cd openfisca.org
+git fetch  # Make sure we are up to date with git remote branches
 git add .
 git config --global user.name "OpenFisca-Bot"
 git config --global user.email "contact@openfisca.fr"
@@ -14,11 +15,11 @@ if git status --untracked-files=no ; then
 	echo "There was an issue pushing to openfisca.org"
 fi
 # the following line removes all files in the branch exept the doc file.
-git filter-branch --tree-filter 'rm --recursive --force $(git ls-files | grep --invert-match doc)' -- --all
+rm --recursive --force $(git ls-files | grep --invert-match doc)
 # the following lines keep the context while changing branch.
 git checkout --detach
-git reset --soft doc-html
+git reset --soft origin/doc-html
 git checkout doc-html
 git add .
 git commit --message="Push from openfisca doc"
-git push https://github.com/openfisca/openfisca.org.git gh-pages:doc-html
+git push
