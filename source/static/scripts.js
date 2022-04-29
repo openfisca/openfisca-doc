b593b0434d39420196ab742beb7137eb336ccced
Matti Schneider <matti.schneider@beta.gouv.fr>
2022-04-29 16:16:43 +0300

    Fix casing

--- source/static/scripts.js
+++ source/static/scripts.js
@@ -21,7 +21,7 @@ function getPathName() {
 
 $(document).ready(function() {
 
-  $('a[href^="https://"], a[href^="http://"]').attr("target", "_blank"); // Make all external links open in a new Window
+  $('a[href^="https://"], a[href^="http://"]').attr("target", "_blank"); // Make all external links open in a new window
 
   // Activate action button tooltip
   $('.actionbutton').tooltip()
